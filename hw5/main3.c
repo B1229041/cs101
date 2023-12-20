2ffc56f86c41294884ac05586a2e29c70018a16f
B1229041 <144075718+B1229041@users.noreply.github.com>
2023-12-20 22:29:17 +0800

    Create main3.c

---
+++ hw5/main3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=1;
+    int b=1;
+    int i;
+    
+    for(i=1;i<=81;i++)
+    {
+        if(b<=9)
+        {
+            if(a<9)
+            {
+                printf("%d*%d=%d\t",b,a,b*a);
+                a=a+1;
+            }
+            else
+            {
+                printf("%d*%d=%d\n",b,a,b*a);
+                b=b+1;
+                a=1;
+            }
+        }
+    }
+
+    return 0;
+}
