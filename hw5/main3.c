e80b2470d5de83705a4d6e6ffe44145977db04e8
B1229041 <144075718+B1229041@users.noreply.github.com>
2023-12-20 22:41:22 +0800

    Update main3.c

--- hw5/main3.c
+++ hw5/main3.c
@@ -5,18 +5,13 @@ int main()
     int a=1;
     int b=1;
     int i;
-    
-    for(i=1;i<=81;i++)
-    {
-        if(b<=9)
-        {
-            if(a<9)
-            {
+    for(i=1;i<=81;i++){
+        if(b<=9){
+            if(a<9){
                 printf("%d*%d=%d\t",b,a,b*a);
                 a=a+1;
             }
-            else
-            {
+            else{
                 printf("%d*%d=%d\n",b,a,b*a);
                 b=b+1;
                 a=1;
